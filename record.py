b43a2f7ed6e78a9485f341f58b5be7be48f6d01b
iver56 <iver56@hotmail.com>
2018-03-31 15:24:37 +0200

    Visualize audio input RMS in console

--- record.py
+++ record.py
@@ -1,5 +1,5 @@
 """PyAudio example: Record a few seconds of audio and save to a WAVE file."""
-import io
+import math
 import struct
 import wave
 
@@ -9,7 +9,7 @@ SAMPLES_PER_CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-RECORD_SECONDS = 5
+RECORD_SECONDS = 50
 WAVE_OUTPUT_FILENAME = "output.wav"
 
 p = pyaudio.PyAudio()
@@ -25,21 +25,23 @@ stream = p.open(
     frames_per_buffer=SAMPLES_PER_CHUNK
 )
 
-print("* recording")
-
-my_buffer = io.BytesIO()
-
 frames = []
 for i in range(0, int(RATE / SAMPLES_PER_CHUNK * RECORD_SECONDS)):
     data = stream.read(SAMPLES_PER_CHUNK)
 
-    unpacked_ints = []
+    values = []
     for j in range(SAMPLES_PER_CHUNK):
         byte_index = j * BYTES_PER_SAMPLE
         those_bytes = data[byte_index:byte_index + BYTES_PER_SAMPLE]
-        unpacked_int = struct.unpack("<h", those_bytes)
-        unpacked_ints.append(unpacked_int)
-        print(unpacked_int)
+        unpacked_int = struct.unpack("<h", those_bytes)[0]
+        value = unpacked_int / 32767  # between -1 and 1
+        values.append(value)
+
+    rms = math.sqrt(
+        sum(value ** 2 for value in values) / SAMPLES_PER_CHUNK
+    )
+    b = (" " + "#" * int(rms * 79)).ljust(80)
+    print(b, end="\r")
 
     frames.append(data)
 
