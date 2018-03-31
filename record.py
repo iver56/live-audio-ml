48c55d459677ba4f5b4bb2ab0ad02f5dbe89fb7a
iver56 <iver56@hotmail.com>
2018-03-31 13:55:08 +0200

    Unpack wav bytes

--- record.py
+++ record.py
@@ -1,30 +1,46 @@
 """PyAudio example: Record a few seconds of audio and save to a WAVE file."""
-
-import pyaudio
+import io
+import struct
 import wave
 
-CHUNK = 1024
+import pyaudio
+
+SAMPLES_PER_CHUNK = 1024
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
+CHANNELS = 1
 RATE = 44100
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
 
 p = pyaudio.PyAudio()
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+BYTES_PER_SAMPLE = p.get_sample_size(FORMAT)
+BIT_DEPTH = BYTES_PER_SAMPLE * 8
+
+stream = p.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=SAMPLES_PER_CHUNK
+)
 
 print("* recording")
 
-frames = []
+my_buffer = io.BytesIO()
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    print('CHUNK')
+frames = []
+for i in range(0, int(RATE / SAMPLES_PER_CHUNK * RECORD_SECONDS)):
+    data = stream.read(SAMPLES_PER_CHUNK)
+
+    unpacked_ints = []
+    for j in range(SAMPLES_PER_CHUNK):
+        byte_index = j * BYTES_PER_SAMPLE
+        those_bytes = data[byte_index:byte_index + BYTES_PER_SAMPLE]
+        unpacked_int = struct.unpack("<h", those_bytes)
+        unpacked_ints.append(unpacked_int)
+        print(unpacked_int)
+
     frames.append(data)
 
 print("* done recording")
@@ -35,7 +51,7 @@ p.terminate()
 
 wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setsampwidth(BYTES_PER_SAMPLE)
 wf.setframerate(RATE)
 wf.writeframes(b''.join(frames))
 wf.close()
