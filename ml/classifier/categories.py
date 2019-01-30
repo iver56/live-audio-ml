882bc43fed05c1007c63e9169b8a514632eee8be
iver56 <ij@aiascience.com>
2019-01-30 19:04:27 +0100

    Export prepared dataset

---
+++ ml/classifier/categories.py
@@ -0,0 +1,23 @@
+CATEGORIES = [
+    "acoustic_guitar",
+    #"airplane",
+    #"applause",
+    #"bird",
+    #"car",
+    #"child",
+    #"church_bell",
+    #"crowd",
+    #"dog_barking",
+    #"engine",
+    #"fireworks",
+    #"footstep",
+    #"glass_breaking",
+    #"hammer",
+    "helicopter",
+    "knock",
+    "laughter",
+]
+
+
+def is_laughter_category(category_name):
+    return category_name == "laughter"
--- ml/classifier/prepare_data.py
+++ ml/classifier/prepare_data.py
@@ -1,33 +1,66 @@
 import os
 
+import joblib
 import librosa
 import numpy as np
 from scipy.io import wavfile
+from tqdm import tqdm
 
+from ml.classifier.categories import CATEGORIES, is_laughter_category
 from ml.settings import AUDIO_EVENT_DATASET_PATH, DATA_DIR
 from ml.utils.filename import get_file_paths
-from ml.utils.plot import plot_matrix
 
 if __name__ == "__main__":
     plot_dir = DATA_DIR / "plots"
     os.makedirs(plot_dir, exist_ok=True)
 
-    file_paths = get_file_paths(AUDIO_EVENT_DATASET_PATH / "train" / "laughter")
-    for file_path in file_paths:
-        # print(file_path)
-        sample_rate, sound_np = wavfile.read(file_path)
-        # print(sample_rate)
-        # print(sound_np.shape, sound_np.dtype, np.amin(sound_np), np.amax(sound_np))
+    fixed_sound_length = 150  # in "spectrogram" windows
+    num_mels = 100
 
-        sound_np = sound_np / 32767  # ends up roughly between -1 and 1
+    x_sequences = []
+    y_values = []
 
-        spectrogram = librosa.feature.melspectrogram(
-            y=sound_np, sr=sample_rate, n_mels=100, power=1.0
-        )
+    for category in tqdm(CATEGORIES):
+        file_paths = get_file_paths(AUDIO_EVENT_DATASET_PATH / "train" / category)
+        target_value = 1 if is_laughter_category(category) else 0
 
-        # Normalize the sound level and squeeze (compress) the peaks a little
-        normalization_value = np.percentile(spectrogram, 90)
-        spectrogram = np.tanh(spectrogram / normalization_value)
+        for file_path in file_paths:
+            # print(file_path)
+            sample_rate, sound_np = wavfile.read(file_path)
+            # print(sample_rate)
+            # print(sound_np.shape, sound_np.dtype, np.amin(sound_np), np.amax(sound_np))
 
-        print(np.amin(spectrogram), np.amax(spectrogram))
-        plot_matrix(spectrogram, output_image_path=plot_dir / "{}.png".format(file_path.stem))
+            sound_np = sound_np / 32767  # ends up roughly between -1 and 1
+
+            spectrogram = librosa.feature.melspectrogram(
+                y=sound_np, sr=sample_rate, n_mels=num_mels, power=1.0
+            )
+
+            # Normalize the sound level and squeeze (compress) the peaks a little
+            normalization_value = np.percentile(spectrogram, 90)
+            spectrogram = np.tanh(spectrogram / normalization_value)
+
+            # print(np.amin(spectrogram), np.amax(spectrogram))
+            # plot_matrix(spectrogram, output_image_path=plot_dir / "{}.png".format(file_path.stem))
+
+            # Transpose the matrix, because we want to use each column as a feature vector
+            spectrogram = np.transpose(spectrogram)
+
+            vectors = np.zeros(shape=(fixed_sound_length, num_mels), dtype=np.float32)
+            window = spectrogram[:fixed_sound_length]
+            actual_window_length = len(window)  # may be smaller than fixed_sound_length
+            vectors[:actual_window_length] = window
+
+            x_sequences.append(vectors)
+            y_values.append(target_value)
+
+    x_sequences = np.array(x_sequences, dtype=np.float32)
+    y_values = np.array(y_values, dtype=np.float32)
+
+    os.makedirs(DATA_DIR / "prepared_dataset", exist_ok=True)
+    joblib.dump(
+        {"x_sequences": x_sequences, "y_values": y_values},
+        os.path.join(DATA_DIR / "prepared_dataset", "dataset.pkl"),
+        compress=True,
+    )
+    print("Stored dataset")
