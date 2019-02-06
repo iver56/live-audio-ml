a0e6d4f92ca23d15b849ba1d1b8ee7760d0df4cc
Olav Markussen <olavmarkussen@hotmail.no>
2019-02-06 19:18:51 +0100

    Add working ring-buffer recorder

--- ml/ui/ui.py
+++ ml/ui/ui.py
@@ -21,18 +21,17 @@ SAMPLES_PER_CHUNK = 2048
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RECORD_SECONDS = 5000
+NUM_SECONDS = 7
 
 
 BYTES_PER_SAMPLE = p.get_sample_size(FORMAT)
 BIT_DEPTH = BYTES_PER_SAMPLE * 8
 
-fps = 30
-
 pygame.display.init()
 pygame.display.set_mode([640, 480])
 
 samples_ring_buffer = collections.deque(
-    maxlen=int(math.ceil((FIXED_SOUND_LENGTH + FFT_WINDOW_SIZE / HOP_LENGTH) * HOP_LENGTH))
+    maxlen=int(math.ceil((NUM_SECONDS * SAMPLE_RATE)/SAMPLES_PER_CHUNK))
 )
 
 stream = p.open(
@@ -42,20 +41,6 @@ stream = p.open(
     input=True,
     frames_per_buffer=SAMPLES_PER_CHUNK,
 )
-
-# for i in range(0, int(SAMPLE_RATE / SAMPLES_PER_CHUNK * RECORD_SECONDS)):
-#     data = stream.read(SAMPLES_PER_CHUNK)
-#
-#     for j in range(SAMPLES_PER_CHUNK):
-#         byte_index = j * BYTES_PER_SAMPLE
-#         those_bytes = data[byte_index : byte_index + BYTES_PER_SAMPLE]
-#         unpacked_int = struct.unpack("<h", those_bytes)[0]
-#         value = unpacked_int / 32767  # ends up roughly between -1 and 1
-#         samples_ring_buffer.append(value)
-#
-#     samples = np.array(samples_ring_buffer)
-#     spectrogram = preprocess_audio_chunk(samples)
-# stream.stop_stream()
 
 
 def main_ui_loop():
@@ -74,6 +59,7 @@ def main_ui_loop():
                     waveFile.setframerate(SAMPLE_RATE)
                     waveFile.writeframes(b''.join(samples_ring_buffer))
                     waveFile.close()
+                    print("We have saved a file")
 
                 if event.key == pygame.K_1:
                     print("Doing whatever")
