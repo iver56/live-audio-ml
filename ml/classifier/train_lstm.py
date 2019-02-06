28ac3f91649d81c9db34a0a0f97ece2930b4ff46
iver56 <ij@aiascience.com>
2019-02-06 20:12:37 +0100

    Fix LSTM model filename

--- ml/classifier/train_lstm.py
+++ ml/classifier/train_lstm.py
@@ -59,5 +59,5 @@ if __name__ == "__main__":
 
     os.makedirs(DATA_DIR / "models", exist_ok=True)
 
-    model.save(os.path.join(DATA_DIR / "models", "tcn.h5"))
+    model.save(os.path.join(DATA_DIR / "models", "lstm.h5"))
     print("Model is saved")
