78890d1149597db5cbe256486bfa5c3bf6017890
iver56 <iver56@hotmail.com>
2019-02-24 12:57:12 +0100

    Fix train_lstm after data generator behavior changed

--- ml/classifier/train_lstm.py
+++ ml/classifier/train_lstm.py
@@ -8,14 +8,19 @@ from keras.optimizers import RMSprop
 from ml.classifier.data_generator import (
     get_train_paths,
     get_validation_paths,
-    SoundExampleGenerator)
+    SoundExampleGenerator,
+)
 from ml.settings import DATA_DIR
 
 
-def get_lstm_model(input_vector_size, target_vector_size=1, dropout0=0.1, learning_rate=0.001):
+def get_lstm_model(
+    input_vector_size, target_vector_size=1, dropout0=0.1, learning_rate=0.001
+):
     model = Sequential()
     model.add(
-        TimeDistributed(Dense(32, activation="relu"), input_shape=(None, input_vector_size))
+        TimeDistributed(
+            Dense(32, activation="relu"), input_shape=(None, input_vector_size)
+        )
     )
     model.add(TimeDistributed(Dropout(dropout0)))
     model.add(CuDNNLSTM(units=32, return_sequences=True))
@@ -24,9 +29,7 @@ def get_lstm_model(input_vector_size, target_vector_size=1, dropout0=0.1, learni
     model.add(Activation("relu"))
     model.add(Dense(target_vector_size, activation="sigmoid"))
     model.compile(
-        loss="binary_crossentropy",
-        optimizer=RMSprop(lr=learning_rate),
-        metrics=["acc"],
+        loss="binary_crossentropy", optimizer=RMSprop(lr=learning_rate), metrics=["acc"]
     )
     # model.summary()
     return model
@@ -35,17 +38,22 @@ def get_lstm_model(input_vector_size, target_vector_size=1, dropout0=0.1, learni
 if __name__ == "__main__":
     train_paths = get_train_paths()
     fixed_sound_length = 150
+    num_mels = 20
     train_generator = SoundExampleGenerator(
-        train_paths, fixed_sound_length=fixed_sound_length, num_mels=20
+        train_paths, fixed_sound_length=fixed_sound_length, num_mels=num_mels
     )
-    train_sample_batch_x, train_sample_batch_y = next(train_generator)
-    n_timesteps = len(train_sample_batch_x[0])
+    train_sample_batch_x, train_sample_batch_y = train_generator[0]
     input_vector_size = len(train_sample_batch_x[0][0])
 
     validation_paths = get_validation_paths()
     validation_generator = SoundExampleGenerator(
-        validation_paths, fixed_sound_length=fixed_sound_length, num_mels=20, augment=False
+        validation_paths,
+        batch_size=192,
+        fixed_sound_length=fixed_sound_length,
+        num_mels=num_mels,
+        augment=False,
     )
+    validation_data = validation_generator[0]
 
     model = get_lstm_model(input_vector_size)
 
@@ -58,8 +66,7 @@ if __name__ == "__main__":
 
     model.fit_generator(
         train_generator,
-        validation_data=validation_generator,
-        validation_steps=192,
+        validation_data=validation_data,
         steps_per_epoch=192,
         epochs=50,
         shuffle=False,
