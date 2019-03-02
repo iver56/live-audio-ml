a3124da82e321c65040df2914a65aa46dd986747
iver56 <iver56@hotmail.com>
2019-03-02 16:09:57 +0100

    Add support for float32 wav files

--- ml/classifier/prepare_data.py
+++ ml/classifier/prepare_data.py
@@ -65,7 +65,8 @@ def load_wav_file(sound_file_path):
             )
         )
 
-    assert sound_np.dtype == np.int16
+    if sound_np.dtype != np.float32:
+        assert sound_np.dtype == np.int16
+        sound_np = sound_np / 32767  # ends up between -1 and 1
 
-    sound_np = sound_np / 32767  # ends up roughly between -1 and 1
     return sound_np
